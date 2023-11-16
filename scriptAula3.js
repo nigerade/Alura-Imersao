2142260b5b96a28ce28ffd2a571715169f6796c0
Nicholas Gerade <nicholasgerade@gmail.com>
2023-11-15 22:44:12 -0300

    Aula 3

---
+++ scriptAula3.js
@@ -0,0 +1,17 @@
+/*
+var filmeRafa = "https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg";
+var filmePaulo = "https://static.wixstatic.com/media/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg/v1/fill/w_640,h_998,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg";
+var filmeGui = "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg";
+
+var listaFilmes = [
+    "https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg",
+    "https://static.wixstatic.com/media/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg/v1/fill/w_640,h_998,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg",
+    "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/7/76/10_Things_I_Hate_About_You.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/c/c1/The_Matrix_Poster.jpg"
+]
+
+for (var i = 0; i < listaFilmes.length; i = i + 1) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+}
+*/
